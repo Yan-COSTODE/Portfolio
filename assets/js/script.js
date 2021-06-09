5daa60e506e1f8e1e467377b1d8b95e4768cbf8f
Yan Costode <74534098+Yan-COSTODE@users.noreply.github.com>
2021-06-09 14:42:21 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+  var vid = document.getElementById("myVideo");
+  vid.autoplay = true;
+  vid.load();
+  
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+       $("header").addClass("changeColor");
+    } else {
+       $("header").removeClass("changeColor");
+    }
+  });
+});
